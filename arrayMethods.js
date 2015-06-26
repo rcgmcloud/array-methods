187e449b29c106ce86676b95097c4c7402145f44
Raymond Mariano <rcm@hawaii.edu>
2015-06-25 18:32:51 -1000

    18 passes

--- arrayMethods.js
+++ arrayMethods.js
@@ -26,3 +26,23 @@ genericNumberArray.push(6, 7, 8, 9, 10);
 var popResult = colors.pop('Charlie');
 
 scrambledWords.reverse();
+phoneNumber.reverse();
+
+var nextOrder = orderQueue.shift();
+
+mixedNums.sort();
+mixedWords.sort();
+
+var notFruit = fruitCollection.splice(1, 3);
+
+gemBox.splice(5, 2);
+gemBox.splice(6);
+
+upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8);
+
+brownOnly.splice(1, 5);
+brownOnly.splice(1, "brown", "brown", "brown", "brown");
+
+var orderedValuesNewLength = orderedValues.unshift(1, 2, 3);
+
+var randomThingsArray = genericNumberArray.concat(colors);
