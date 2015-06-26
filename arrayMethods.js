a4f682fe86e88db08f2163f58ace9e29b5c101f9
Raymond Mariano <rcm@hawaii.edu>
2015-06-25 18:09:42 -1000

    first commit

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,13 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+var nameArray = [];
+
+nameArray.push("Raymond", "Mariano");
+
+genericNumberArray.push(6, 7, 8, 9, 10);
+
+var popResult = colors.pop('Charlie');
+
+scrambledWords.reverse();
